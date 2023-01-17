3281de5694a48c03982feb7f32809f789aa2ab07
Matthew Bonnecaze <bonnecaze.matthew@gmail.com>
2023-01-17 17:45:38 -0500

    Play Suspicion Audio during exam

--- EGD-Project-01/Assets/Scripts/Suspicion.cs
+++ EGD-Project-01/Assets/Scripts/Suspicion.cs
@@ -1,12 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class Suspicion : MonoBehaviour
 {
+    [SerializeField] float suspicionTime = 1.5f;
     [SerializeField] Exam exam;
+    AudioSource audioSource;
+    [SerializeField] AudioClip audioNormal, audioSuspenseful;
+    [SerializeField] Text timerText;
+    /*[SerializeField] VideoPlayer videoPlayer;
+    [SerializeField] */
     bool viewingCheatSheet = false;
-    float suspicion = 0.0f;
+    
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -21,13 +34,43 @@ public class Suspicion : MonoBehaviour
         {
             exam.TabCheatSheet();
             viewingCheatSheet = true;
+            // Increase Time Scale
+            Time.timeScale = suspicionTime;
+            // Change audio
+            ChangeAudioClip(audioSuspenseful);
+            // Change Timer Color
+            ChangeTimeTextColor();
+            // Change video
         }
 
         if (Input.GetKeyUp(KeyCode.Tab))
         {
             exam.TabCheatSheet();
             viewingCheatSheet = false;
+            Time.timeScale = 1;
+            ChangeAudioClip(audioNormal);
+            ChangeTimeTextColor();
         }
 
+    }
+
+    private void ChangeAudioClip(AudioClip clip)
+    {
+        float audioTime = audioSource.time;
+        audioSource.clip = clip;
+        audioSource.Play();
+        audioSource.time = audioTime;
+    }
+
+    private void ChangeTimeTextColor()
+    {
+        if (viewingCheatSheet)
+        {
+            timerText.color = Color.red;
+        }
+        else
+        {
+            timerText.color = Color.black;
+        }
     }
 }
